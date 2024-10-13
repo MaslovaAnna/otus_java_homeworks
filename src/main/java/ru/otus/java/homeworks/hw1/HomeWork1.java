0730e66399b68fa1eed8ec9cb3e5b11ef7724692
YaGrib <masl_01@mail.ru>
2024-10-13 23:48:06 +0300

    Сократила условия, разобралась почему было предупреждение(как раз избыточное условие)

--- src/main/java/ru/otus/java/homeworks/hw1/HomeWork1.java
+++ src/main/java/ru/otus/java/homeworks/hw1/HomeWork1.java
@@ -68,9 +68,9 @@ public class HomeWork1 {
         System.out.println(data);
         if (data <= 10) {
             System.out.println("Красный");
-        } else if (data > 10 && data < 20) {
+        } else if (data <= 20) {
             System.out.println("Желтый");
-        } else if (data > 20) {
+        } else {
             System.out.println("Зеленый");
         }
     }
