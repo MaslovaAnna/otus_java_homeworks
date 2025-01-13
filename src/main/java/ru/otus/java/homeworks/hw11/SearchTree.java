7d7456badb7e1b894f1b42774094a56bc64a5f0e
YaGrib <masl_01@mail.ru>
2025-01-13 20:11:00 +0300

    ДЗ 11. правки

--- src/main/java/ru/otus/java/homeworks/hw11/SearchTree.java
+++ src/main/java/ru/otus/java/homeworks/hw11/SearchTree.java
@@ -2,7 +2,7 @@ package ru.otus.java.homeworks.hw11;
 
 import java.util.List;
 
-public interface SearchTree<Integer> {
+public interface SearchTree {
     Integer find(Integer element);
     List<Integer> getSortedList();
 }
