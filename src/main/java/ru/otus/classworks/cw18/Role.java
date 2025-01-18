2d8d75120d14c40368b5dab0f6f039af372c222e
YaGrib <masl_01@mail.ru>
2025-01-18 16:59:51 +0300

    ДЗ 19. Работа с дженериками

--- src/main/java/ru/otus/classworks/cw18/Role.java
+++ src/main/java/ru/otus/classworks/cw18/Role.java
@@ -1,4 +1,4 @@
-package main.java.ru.otus.java.classworks.cw18;
+package main.java.ru.otus.classworks.cw18;
 
 public class Role {
 
---
+++ src/main/java/ru/otus/homeworks/hw19/Orange.java
@@ -0,0 +1,13 @@
+package main.java.ru.otus.homeworks.hw19;
+
+public class Orange extends Fruit {
+    @Override
+    public String toString() {
+        return "Orange";
+    }
+
+    public Orange() {
+        super(4);
+
+    }
+}
