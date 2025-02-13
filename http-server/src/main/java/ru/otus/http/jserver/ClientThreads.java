07f0ddf658736c657c523e03d3123eb22475e3cf
YaGrib <masl_01@mail.ru>
2025-02-13 14:07:42 +0300

    ДЗ 23. Остался 1 вопрос про то, как можно закрыть сервер по времени(например долгое ожидание запросов)

---
+++ http-server/src/main/java/ru/otus/http/jserver/ClientThreads.java
@@ -0,0 +1,57 @@
+package ru.otus.http.jserver;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+public class ClientThreads {
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public ClientThreads(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = new DataInputStream(socket.getInputStream());
+        this.out = new DataOutputStream(socket.getOutputStream());
+        Dispatcher dispatcher = new Dispatcher();
+            try {
+                System.out.println("Подключился новый клиент");
+                byte[] buffer = new byte[8192];
+                int n = socket.getInputStream().read(buffer);
+                HttpRequest request = new HttpRequest(new String(buffer, 0, n));
+                request.info(true);
+                dispatcher.execute(request, socket.getOutputStream());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+        }
+            finally {
+                disconnect();
+            }
+
+    }
+    public void disconnect() {
+        try {
+            if (in != null) {
+                in.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (out != null) {
+                out.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- http-server/src/main/java/ru/otus/http/jserver/HttpServer.java
+++ http-server/src/main/java/ru/otus/http/jserver/HttpServer.java
@@ -3,29 +3,34 @@ package ru.otus.http.jserver;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private int port;
-    private Dispatcher dispatcher;
 
     public HttpServer(int port) {
         this.port = port;
-        this.dispatcher = new Dispatcher();
     }
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
-            try (Socket socket = serverSocket.accept()) {
-                System.out.println("Подключился новый клиент");
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                HttpRequest request = new HttpRequest(new String(buffer, 0, n));
-                request.info(true);
-                dispatcher.execute(request, socket.getOutputStream());
+            while (!serverSocket.isClosed()) {
+            Socket socket = serverSocket.accept();
+                ExecutorService socketsPool = Executors.newCachedThreadPool();
+                socketsPool.execute(() -> {
+                    try {
+                        new ClientThreads(socket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
+            
         } catch (IOException e) {
-            e.printStackTrace();
-        }
+                e.printStackTrace();
+
+            }
     }
 }
